c3070ddf484ef973a7b58476ed173e14c39d442d
Schmaexxi <langknecht@stud.uni-heidelberg.de>
2021-09-12 18:48:17 +0200

    add tests, solve task 1

---
+++ annotator_and_reference_set_analysis.py
@@ -0,0 +1,133 @@
+import json
+import pandas
+import pandas as pd
+from typing import List
+import matplotlib.pyplot as plt  # type: ignore
+import matplotlib.ticker as ticker  # type: ignore
+from pathlib import Path
+
+
+def plot_annotation_times(
+    df: pd.DataFrame, title: str = 'Annotation duration times', f_name: str = 'annotation_duration'
+) -> None:
+    """
+    Plots a figure with a box plot and scatter plot for a column 'task_output_duration_ms'
+    :param df: dataframe
+    :param title: title for plot
+    :param f_name: file_name to use
+    """
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 6), dpi=300)
+    plt.subplots_adjust(wspace=0.3)
+    fig.suptitle(f'{title}')
+    ax1.boxplot(df['task_output_duration_ms'], showfliers=False, notch=True)
+    ax1.get_xaxis().set_visible(False)
+    ax1.set_title('Box plot for all annotator times')
+    ax1.set_ylabel('Time in ms')
+    # use index values for x-axis (annotators)
+    ax2.scatter([index for index in list(df.index.get_level_values(0))], df['task_output_duration_ms'], s=1)
+    ax2.xaxis.set_major_locator(ticker.MultipleLocator(1))  # show every annotator on the x-axis
+    ax2.set_title('Annotation duration by annotator')
+    ax2.set_xlabel('Annotator')
+    ax2.set_ylabel('Time in ms')
+    ax2.tick_params(axis='x', labelsize=8)
+    (Path.cwd() / 'plots').mkdir(parents=True, exist_ok=True)
+    fig.savefig(Path.cwd() / f'plots/{f_name}.png')
+
+
+with open("data/anonymized_project.json", "r+") as f:
+    data: dict = json.load(f)
+result_set: dict = data['results']['root_node']['results']
+
+column_names: List = [
+    ('created_at',),
+    ('workpackage_total_size',),
+    ('loss',),
+    ('task_output', 'answer'),
+    (
+        'task_output',
+        'cant_solve',
+    ),
+    (
+        'task_output',
+        'corrupt_data',
+    ),
+    (
+        'task_output',
+        'duration_ms',
+    ),
+    (
+        'user',
+        'vendor_id',
+    ),
+    (
+        'user',
+        'vendor_user_id',
+    ),
+]
+selected_data: List = []
+
+#  iterate task results
+for task_pseudo, task_stats in result_set.items():
+    # print(annotator, task_list)
+    #  iterate list of annotators for this task
+    for annotation in task_stats['results']:
+
+        annotation_stats: List = []
+
+        # index.append([annotation['user']['vendor_id'], annotation['user']['vendor_user_id']])
+
+        for c_name in column_names:
+            if len(c_name) > 1:
+                annotation_stats.extend([annotation[c_name[0]][c_name[1]]])
+            else:
+                annotation_stats.extend([annotation[c_name[0]]])
+        annotation_stats.append(task_pseudo)  # add task _pseudonym
+        selected_data.append(annotation_stats)
+
+column_names.append(('task_pseudonym',))
+col_names: List = [c_name[0] if len(c_name) == 1 else f"{c_name[0]}_{c_name[1]}" for c_name in column_names]
+annotator_df: pd.DataFrame = pd.DataFrame(selected_data, columns=col_names)
+annotator_df['user_id'] = [int(user.rsplit('_')[-1]) for user in annotator_df['user_vendor_user_id']]
+annotator_df = annotator_df.set_index('user_id')
+annotator_df.sort_index(inplace=True)
+
+annotator_count: int = len(set(list(annotator_df.index.get_level_values(0))))  # number of distinct annotators
+
+dur_max: int = annotator_df['task_output_duration_ms'].max()  # maximum annotation duration
+dur_min: int = annotator_df['task_output_duration_ms'].min()  # minimum annotation duration
+dur_mean: int = annotator_df['task_output_duration_ms'].mean()  # average annotation duration
+plot_annotation_times(annotator_df)
+
+curated_df: pd.DataFrame = annotator_df[(annotator_df['task_output_duration_ms'] > 0)]
+plot_annotation_times(
+    curated_df, title='Annotation duration times without negatives', f_name='annotation_duration_without_negatives'
+)
+
+curated_df = annotator_df[(annotator_df['task_output_duration_ms'] > 250)]
+plot_annotation_times(
+    curated_df,
+    title='Annotation duration times below average reaction time',
+    f_name='annotation_duration_below_average_reaction_time',
+)
+
+# respective number of results produced by individual annotators
+annotator_result_count: dict = {
+    index: len(annotator_df.loc[index]) for index in set(list(annotator_df.index.get_level_values(0)))
+}
+fig = plt.figure(figsize=(6, 3), dpi=150)
+plt.bar(annotator_result_count.keys(), annotator_result_count.values(), color='#a1ccf4')
+plt.title('Annotations by annotator')
+plt.xlabel('Annotator')
+plt.ylabel('Number of annotations')
+fig.savefig(Path.cwd() / 'plots/annotator_results.png')
+
+answer_counts_by_question: pandas.Series = annotator_df.groupby('task_pseudonym')['task_output_answer'].value_counts()
+
+controversial_questions: List = []
+for index, count in answer_counts_by_question.items():
+    # do annotators vote 'yes' nearly as often as 'no' - adjust this range to define the term "highly disagree"
+    if count in range(4, 6):
+        if index[0] not in controversial_questions:
+            controversial_questions.append(index[0])
+
+print(len(controversial_questions))
---
+++ tests/test_data_consistency.py
@@ -0,0 +1,84 @@
+import unittest
+from pathlib import Path
+import json
+import pandas as pd
+
+
+class TestFileStructure(unittest.TestCase):
+    def setUp(self) -> None:
+        """
+        Loads a json-file and assigns a nested dictionary accessed by the keys "['results']['root_node']['results']"
+        """
+        path_to_data: Path = Path.cwd().parent / 'data/anonymized_project.json'
+        with open(path_to_data, 'r') as file:
+            data: dict = json.load(file)
+        self.result_set: dict = data['results']['root_node']['results']
+
+    def test_consistent_hierarchy(self) -> None:
+        """
+        Checks a dictionary's structure for coherence,
+        i.e., the existence of the same keys
+        """
+        # get arbitrary first task from dict
+        arbitrary_first_task: str = list(self.result_set.keys())[0]
+        arbitrary_first_result_set: dict = self.result_set.pop(arbitrary_first_task)
+        arbitrary_first_result: dict = arbitrary_first_result_set['results'][0]
+
+        # define dictionary to compare keys of an item with
+        # for nested dicts, their original key holds a list of its nested dictionary's keys
+        fields: dict = {
+            key: [nested_key for nested_key in val.keys()]
+            for key, val in arbitrary_first_result.items()
+            if isinstance(val, dict)
+        }
+
+        fields['base'] = [key for key in arbitrary_first_result.keys()]  # first-level keys are accessed by "base"
+
+        # iterate over the input data to assert the existence of the same keys
+        for _, task_list in self.result_set.items():
+            for task in task_list['results']:
+                for key, val in task.items():
+                    # check the keys in every task
+                    self.assertEqual([key for key in task.keys()], fields['base'])
+                    if isinstance(val, dict):  # for keys of nested dicts
+                        self.assertEqual(
+                            [key for key in val.keys()], fields[key], "Keys of nested json field do not match"
+                        )
+
+    def test_gui_type(self) -> None:
+        """
+        Checks a dictionary for mismatching gui_types
+        """
+        # get arbitrary first gui type from dict
+        arbitrary_first_gui_type: str = self.result_set[list(self.result_set.keys())[0]]['gui_type']
+
+        for _, results in self.result_set.items():
+            self.assertEqual(
+                results['gui_type'], arbitrary_first_gui_type, f"{results['gui_type']} != {arbitrary_first_gui_type}"
+            )
+
+    def test_id_user_relation(self) -> None:
+        """
+        Checks for a dictionary whether the keys 'vendor_id' and 'vendor_user_id' uniquely identify the key 'id'
+        Assumes 'test_consistent_hierarchy' to succeed
+        """
+        users: dict = {
+            'vendor_id': [],
+            'id': [],
+            'vendor_user_id': [],
+        }
+        # populate the defined dict with users from the results
+        for question_result in self.result_set.values():
+            for result in question_result['results']:
+                for key, val in result['user'].items():
+                    users[key].append(val)
+        users_and_ids_df: pd.DataFrame = pd.DataFrame.from_dict(users)
+        individual_users: set = set(list(users_and_ids_df['vendor_user_id']))  # distinct users
+        # combination of 'id' and 'vendor_user_id'
+        users_and_ids_count_df: pd.DataFrame = users_and_ids_df.groupby(['id', 'vendor_user_id']).count()
+        # is the combination between 'id' and 'vendor_user_id' unique?
+        self.assertTrue(len(users_and_ids_count_df) == len(individual_users))
+
+
+if __name__ == '__main__':
+    unittest.main()
